eaaf4097784b32e19cc142a6d3432bf0a66a4315
Zafranudin Zafrin <mohd.zafranudin@gmail.com>
2022-07-16 21:14:24 +0200

    Feat: Added product service

--- services/product/src/app.controller.ts
+++ services/product/src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Product } from './product.entity';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getProducts(): Product[] {
+    return this.appService.getAll();
+  }
+
+  @Post()
+  createProduct(@Body() product: Product): Product {
+    return this.appService.create(product);
   }
 }
--- services/product/src/app.service.ts
+++ services/product/src/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { Product } from './product.entity';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  products: Product[] = [];
+
+  getAll(): Product[] {
+    return this.products;
+  }
+
+  create(product: Product): Product {
+    const newProduct = {
+      id: this.products.length + 1,
+      ...product
+    };
+
+    this.products.push(newProduct);
+
+    return newProduct;
   }
 }
